41b7d956a1cde233a4ad71619a162107532e406f
Dragan <dragan@jl.is>
2020-09-24 17:40:23 +0200

    Fixed gatsby-config file.

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
          * The full URL of the WordPress site's GraphQL API.
          * Example : 'https://www.example-site.com/graphql'
          */
-        url: process.env.GATSBY_WP_URL,
+        url: `${process.env.GATSBY_WP_URL}/graphql`,
       },
     },
     {
