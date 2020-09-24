b5d4ee5ac0a5a9ed0ccea54e030ae8c431563022
Dragan <dragan@jl.is>
2020-09-24 17:34:04 +0200

    Adding GATSBY_WP_URL to .env.

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
          * The full URL of the WordPress site's GraphQL API.
          * Example : 'https://www.example-site.com/graphql'
          */
-        url: process.env.GATSBY_BACKEND_URL || `http://netlify-test.jonssonlemacks.com/graphql`,
+        url: process.env.GATSBY_WP_URL,
       },
     },
     {
